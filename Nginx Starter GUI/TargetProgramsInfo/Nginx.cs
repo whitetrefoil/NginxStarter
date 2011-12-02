9497698868c2fd5877cef5c93dd768db9910e7dd
WhiteTrefoil <whitetrefoil@gmail.com>
2011-12-02 23:21:59 +0800

    Added standalone classes of target programs information

---
+++ Nginx Starter GUI/TargetProgramsInfo/Nginx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace NginxStarterGUI.TargetProgramsInfo
+{
+	class Nginx
+	{
+	}
+}
