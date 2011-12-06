302ee152c2b6ab76ca7ba549c80b1a57aab9497c
WhiteTrefoil <whitetrefoil@gmail.com>
2011-12-06 15:46:52 +0800

    added test logic

--- FindInPath/FindInPath.cs
+++ FindInPath/FindInPath.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace FindInPath
 {
-    public class FindInPath
+    public static class FindInPath
     {
+        public static string SystemPath = System.Environment.GetEnvironmentVariable("Path");
 
+        public static List<string> GetSystemPaths()
+        {
+            List<string> SystemPaths = new List<string>();
+            SystemPaths.AddRange((SystemPath + ";").Split(';'));
+            return SystemPaths;
+        }
     }
 }
