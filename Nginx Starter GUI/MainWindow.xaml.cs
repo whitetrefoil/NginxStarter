9f650e956c98e3eafcfc2a3ad89fdbace1b0c4de
WhiteTrefoil <whitetrefoil@gmail.com>
2011-12-07 10:19:50 +0800

    clean the "using"s

--- Nginx Starter GUI/MainWindow.xaml.cs
+++ Nginx Starter GUI/MainWindow.xaml.cs
@@ -1,23 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 using System.Xml.Serialization;
 using Microsoft.Win32;
 using NginxStarterGUI.TargetProgramsInfo;
-using NginxStarterGUI.Classes;
 
 namespace NginxStarterGUI
 {
