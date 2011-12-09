f9cbcd43ccba4ce782a5c5bb0608f5ee2c7c8124
WhiteTrefoil <whitetrefoil@gmail.com>
2011-12-09 13:21:59 +0800

    Added a class to convert path format between Win and Unix

---
+++ Nginx Starter GUI/Classes/PathConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NginxStarterGUI.Classes
+{
+	public static class PathConverter
+	{
+		public static string ConvertWinToUnix(string input)
+		{
+			return input.Replace('\\', '/');
+		}
+
+		public static string ConvertUnixToWin(string input)
+		{
+			return input.Replace('/', '\\');
+		}
+	}
+}
