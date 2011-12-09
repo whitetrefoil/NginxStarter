9eac686af9ef3f27a5b1b989e9f7be0f7ceba239
WhiteTrefoil <whitetrefoil@gmail.com>
2011-12-09 14:35:34 +0800

    remove some unnecessary using

--- Nginx Starter GUI/Classes/PathConverter.cs
+++ Nginx Starter GUI/Classes/PathConverter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NginxStarterGUI.Classes
+﻿namespace NginxStarterGUI.Classes
 {
 	public static class PathConverter
 	{
