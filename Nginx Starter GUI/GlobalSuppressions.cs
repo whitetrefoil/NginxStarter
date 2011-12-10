c9b493cd67f115458cefa228e412c77ba3cb90e5
WhiteTrefoil <whitetrefoil@gmail.com>
2011-12-11 02:12:50 +0800

    Added code analyzer ruleset file

---
+++ Nginx Starter GUI/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// 代码分析使用此文件来维护应用到此项目的 SuppressMessage 
+// 特性。
+// 项目级禁止显示或者没有目标，或者已给定 
+// 一个特定目标且其范围为命名空间、类型和成员等。
+//
+// 若要向此文件添加禁止显示，请右击 
+// 错误列表中的消息，再指向“禁止显示消息”，然后单击 
+//“在项目禁止显示文件中”。
+// 无需手动向此文件添加禁止显示。
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "GUI", Scope = "namespace", Target = "NginxStarterGUI.Classes")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "GUI", Scope = "namespace", Target = "NginxStarterGUI.Converters")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "GUI", Scope = "namespace", Target = "NginxStarterGUI.Settings")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "GUI", Scope = "namespace", Target = "NginxStarterGUI.TargetProgramsInfo")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "GUI")]
