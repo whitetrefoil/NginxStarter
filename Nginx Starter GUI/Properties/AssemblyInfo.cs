7a4e3a8b84d05170b3be68e1f46a4b7547fcaba7
WhiteTrefoil <whitetrefoil@gmail.com>
2012-03-05 15:17:25 +0800

    update version number to 0.0.5.4

--- Nginx Starter GUI/Properties/AssemblyInfo.cs
+++ Nginx Starter GUI/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("0.0.5.3")]
+[assembly: AssemblyFileVersion("0.0.5.4")]
 [assembly: NeutralResourcesLanguageAttribute("zh-Hans")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ComparePathTest")]
